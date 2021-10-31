49964d5e5b76c670ca27bb917a8dca0bdcdd1539
maurojcordoba <mauro.j.cordoba@gmail.com>
2021-10-31 12:36:21 -0300

    test

--- bercomat.py
+++ bercomat.py
@@ -7,132 +7,133 @@ import random
 from datetime import datetime,timedelta
 import os
 
+
 MONGO_URI = os.getenv('MONGO_URI')
+def process():
+    client = MongoClient(MONGO_URI)
+    db = client['bercomat']
+    col_productos = db['productos']
 
-client = MongoClient(MONGO_URI)
-db = client['bercomat']
-col_productos = db['productos']
+    # Abro archivo de urls
+    f = open("data.txt", "rt")
+    lista_urls = f.readlines()
+    f.close()
 
-# Abro archivo de urls
-f = open("data.txt", "rt")
-lista_urls = f.readlines()
-f.close()
+    # Fecha/Hora de proceso
+    #created = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    created = datetime.now()
 
-# Fecha/Hora de proceso
-#created = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-created = datetime.now()
+    for x in lista_urls:
+        url = x.rstrip('\n') + '?page='
+        #url = 'https://www.familiabercomat.com/banos/?page='
+        print(url)
 
-for x in lista_urls:
-    url = x.rstrip('\n') + '?page='
-    #url = 'https://www.familiabercomat.com/banos/?page='
-    print(url)
+        #lista completa
+        lista = []
 
-    #lista completa
-    lista = []
-
-    try:
-        # Ultima pagina
-        cookies_dict = {"carea": "S08OlRPNuMSA/Z9yFRkYEogl1LeV7xoGny+7caw/c13cXpVCO4O+HOIC5yUtbZ/f"}
-        page = requests.get(url + "1",cookies=cookies_dict)
-        soup = BeautifulSoup(page.content, 'html.parser')
-        paginacion = soup.find('ul', class_ = 'pager-list reset')
-
-        list_li = paginacion.find_all('li')
-        for li in list_li:pass
-        last_page = int(li.text)
-        
-        # Recorro las paginas
-        for x in range(1, last_page+1):
-            page = requests.get(url+str(x),cookies=cookies_dict)
+        try:
+            # Ultima pagina
+            cookies_dict = {"carea": "S08OlRPNuMSA/Z9yFRkYEogl1LeV7xoGny+7caw/c13cXpVCO4O+HOIC5yUtbZ/f"}
+            page = requests.get(url + "1",cookies=cookies_dict)
             soup = BeautifulSoup(page.content, 'html.parser')
-            productos = soup.find_all('div', class_ = 'l-products-item')
+            paginacion = soup.find('ul', class_ = 'pager-list reset')
 
-            for producto in productos:    
-                # data
-                info = producto.find('div', attrs={"class":"product-tile"})    
-                data = json.loads((info['data-tracking-data']))
-
-                # thumbnail
-                thumbnail = producto.find('span', attrs={"class":"thumbnail"})    
-                data['thunbnail'] = thumbnail.img['data-src']    
-
-                # stock
-                stock = producto.find('span', class_="stock-indication").text.strip()
-                data['stock'] = stock
-
-                # link
-                link = producto.find('a', class_="hyp-thumbnail")
-                data['link'] = link['href']
-
-                # created
-                data['created'] = created
-
-                lista.append(data)           
+            list_li = paginacion.find_all('li')
+            for li in list_li:pass
+            last_page = int(li.text)
             
-            time.sleep(random.uniform(1.0,2.0))
-    except Exception:
-        print("Error en pagina {0}! Continua con la siguiente.".format(url))
-        pass
+            # Recorro las paginas
+            for x in range(1, last_page+1):
+                page = requests.get(url+str(x),cookies=cookies_dict)
+                soup = BeautifulSoup(page.content, 'html.parser')
+                productos = soup.find_all('div', class_ = 'l-products-item')
 
-    # Si la lista no esta vacia, inserto en base
-    if lista:
-        col_productos.insert_many(lista)
+                for producto in productos:    
+                    # data
+                    info = producto.find('div', attrs={"class":"product-tile"})    
+                    data = json.loads((info['data-tracking-data']))
 
-# Ofertas
+                    # thumbnail
+                    thumbnail = producto.find('span', attrs={"class":"thumbnail"})    
+                    data['thunbnail'] = thumbnail.img['data-src']    
 
-fechas = db.productos.distinct("created")
-fechas.sort(reverse=True)
-ult_proceso = fechas[0]
+                    # stock
+                    stock = producto.find('span', class_="stock-indication").text.strip()
+                    data['stock'] = stock
+
+                    # link
+                    link = producto.find('a', class_="hyp-thumbnail")
+                    data['link'] = link['href']
+
+                    # created
+                    data['created'] = created
+
+                    lista.append(data)           
+                
+                time.sleep(random.uniform(1.0,2.0))
+        except Exception:
+            print("Error en pagina {0}! Continua con la siguiente.".format(url))
+            pass
+
+        # Si la lista no esta vacia, inserto en base
+        if lista:
+            col_productos.insert_many(lista)
+
+    # Ofertas
+
+    fechas = db.productos.distinct("created")
+    fechas.sort(reverse=True)
+    ult_proceso = fechas[0]
 
 
-today = datetime.today().replace(hour=0,minute=0,second=0)
-yesterday = today - timedelta(days=1)
+    today = datetime.today().replace(hour=0,minute=0,second=0)
+    yesterday = today - timedelta(days=1)
 
-fechas =  db.productos.distinct("created", {"created": {"$lt": yesterday}})
-fechas.sort(reverse=True)
-pen_proceso = fechas[0]
+    fechas =  db.productos.distinct("created", {"created": {"$lt": yesterday}})
+    fechas.sort(reverse=True)
+    pen_proceso = fechas[0]
 
-print(ult_proceso,pen_proceso)
+    print(ult_proceso,pen_proceso)
 
-### Genero nueva collections con los ultimos productos procesados ###
+    ### Genero nueva collections con los ultimos productos procesados ###
 
-result = db.productos.aggregate([{"$match": { "created": { "$eq": ult_proceso },"stock": {"$ne" : "Agotado"}}},
-    {"$out": "productos_hoy"}])
-    
-### Genero nueva collections con los productos anteriormente procesados  ###
+    result = db.productos.aggregate([{"$match": { "created": { "$eq": ult_proceso },"stock": {"$ne" : "Agotado"}}},
+        {"$out": "productos_hoy"}])
+        
+    ### Genero nueva collections con los productos anteriormente procesados  ###
 
-result = db.productos.aggregate([{"$match": { "created": { "$eq": pen_proceso },"stock": {"$ne" : "Agotado"}}},
-    {"$out": "productos_antes"}])
+    result = db.productos.aggregate([{"$match": { "created": { "$eq": pen_proceso },"stock": {"$ne" : "Agotado"}}},
+        {"$out": "productos_antes"}])
 
-query = [
-{"$project":{"_id":0}},    
-{    "$lookup": {
-        "from": "productos_antes",
-        "let": {"idHoy": "$id", "precioHoy": "$price" },        
-        "pipeline": [
-            { "$match": 
-                { "$expr":
-                    { "$and":
-                        [{"$eq":["$id","$$idHoy"] },{"$gt":["$price", "$$precioHoy"]}]                    
+    query = [
+    {"$project":{"_id":0}},    
+    {    "$lookup": {
+            "from": "productos_antes",
+            "let": {"idHoy": "$id", "precioHoy": "$price" },        
+            "pipeline": [
+                { "$match": 
+                    { "$expr":
+                        { "$and":
+                            [{"$eq":["$id","$$idHoy"] },{"$gt":["$price", "$$precioHoy"]}]                    
+                        }
                     }
-                }
-            },
-            { "$project": { "_id":0,"category":0,"thunbnail":0,"link": 0,"list":0,"name":0}}
-        ],        
-        "as": "antes"
-    }    
-},
-{"$unwind": "$antes"},
-{"$addFields":{ "diff": {"$subtract": ["$antes.price","$price"]}}},
-{"$out": "productos_oferta"}
-]
+                },
+                { "$project": { "_id":0,"category":0,"thunbnail":0,"link": 0,"list":0,"name":0}}
+            ],        
+            "as": "antes"
+        }    
+    },
+    {"$unwind": "$antes"},
+    {"$addFields":{ "diff": {"$subtract": ["$antes.price","$price"]}}},
+    {"$out": "productos_oferta"}
+    ]
 
-result = db.productos_hoy.aggregate(query)
+    result = db.productos_hoy.aggregate(query)
 
-# Genera Historial
-lista = []
-for oferta in db.productos_oferta.find({},{"_id": 0}):
-    lista.append(oferta)
+    # Genera Historial
+    lista = []
+    for oferta in db.productos_oferta.find({},{"_id": 0}):
+        lista.append(oferta)
 
-if lista:
-        db.producto_oferta_hist.insert_many(lista)
+    if lista:
+            db.producto_oferta_hist.insert_many(lista)
--- clock.py
+++ clock.py
@@ -9,6 +9,13 @@ def timed_job():
 @sched.scheduled_job('interval', seconds=20)
 def timed2_job():
     print('Test 20 shutdown.')
-    sched.shutdown(wait=False)
+    #sched.shutdown(wait=False)
+
+def timed3_job():
+    print('Test')
+
+sched.add_job(timed3_job, trigger='cron', hour=15, minute=40)
+
+print(sched.print_jobs())
 
 sched.start()
